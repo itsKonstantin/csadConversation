36abd0c4f5489c47279e1b11011ac750d92e3901
kpschlotter <kps@de.ibm.com>
2018-08-13 15:54:03 +0200

    new version

---
+++ config_sample.js
@@ -0,0 +1,11 @@
+var config = { 
+  watson: {
+   assistant: { 
+      username: "<yourServiceUsername>", 
+      password: "<yourServicePassword>", 
+      version: "2018-07-10",
+      workspace_id: "<yourWorkspaceId>" 
+    } 
+  } 
+}; 
+module.exports = config;
---
+++ routes/assistant.js
@@ -0,0 +1,43 @@
+var config = require('../config');
+var express = require('express'); 
+var bodyParser = require('body-parser');
+var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+var router = express.Router();
+var jsonParser = bodyParser.json();
+
+var watsonAssistant = new AssistantV1(config.watson.conversation);
+
+router.post('/', jsonParser, function(req, res, next) {
+  watsonAssistant.message({ 
+   'input': req.body.input, 
+   'context': req.body.context, 
+   'workspace_id': config.watson.conversation.workspace_id 
+   }, 
+   function(err, response) { 
+     if (err) {
+       console.log('error:', err); 
+     } else { 
+      console.log("Detected input: " + response.input.text);
+      if (response.intents.length > 0) {
+        var intent = response.intents[0];
+        console.log("Detected intent: " + intent.intent);
+        console.log("Confidence: " + intent.confidence);
+      }
+      if (response.entities.length > 0) {
+        var entity = response.entities[0];
+        console.log("Detected entity: " + entity.entity);
+        console.log("Value: " + entity.value);
+        if ((entity.entity === 'help') && (entity.value === 'time')) {
+          var msg = 'The current time is ' + new Date().toLocaleTimeString();
+          console.log(msg);
+          response.output.text = msg;
+        }
+      }
+       // console.log(JSON.stringify(response, null, 2)); 
+      res.json(response);
+     } 
+   }); 
+});
+
+module.exports = router;
