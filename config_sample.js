f507194f7299c669d638646f495a66245b5c7de2
kpschlotter <kps@de.ibm.com>
2018-10-30 19:57:52 +0100

    info for apikey security added

--- config_sample.js
+++ config_sample.js
@@ -1,3 +1,15 @@
+/* In case you have a new service protected by the IAM method use the following code
+var config = { 
+  watson: {
+   assistant: { 
+      iam_apikey: "<yourApiKey>", 
+      version: "2018-07-10",
+      url: "yourServiceUrl",
+      workspace_id: "<yourWorkspaceId>" 
+    } 
+  } 
+}; 
+*/
 var config = { 
   watson: {
    assistant: { 
